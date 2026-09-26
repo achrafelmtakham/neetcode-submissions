54b2b18c31374d87047d71e02c2eb9804816e70e
achrafelmtakham <48456279+achrafelmtakham@users.noreply.github.com>
2026-09-26 11:11:50 +0200

    Add: isomorphic-strings - submission-3

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            s_to_t, t_to_s = {}, {}
+            for char_s, char_t in zip(s,t):
+                if s_to_t.get(char_s, char_t) != char_t or t_to_s.get(char_t, char_s) != char_s:
+                    return False
+                s_to_t[char_s] = char_t
+                t_to_s[char_t] = char_s
+            return True
